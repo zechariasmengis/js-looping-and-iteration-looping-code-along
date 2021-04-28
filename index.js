a470bd6803b68ea3fef9ad545c4d275e97160057
Zecharias Mengis <zmengis@gmail.com>
2021-04-28 10:30:29 -0700

    commit tree 6559aec692eef23b35811e5804c7d3b4823d3abe

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards (array, event) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray
+}
+
+function countDown (integer) {
+    while (integer > 0) {
+        console.log(integer);
+        integer -= 1;
+    }
+    console.log(integer)
+}
